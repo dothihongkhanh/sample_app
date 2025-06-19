74fef863e8587609db498644dd7d45246c02c014
dothihongkhanh <dothihongkhanh22@gmail.com>
2025-06-19 15:44:49 +0700

    feat: complete chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   end
 
   resources :account_activations, only: [ :edit ]
+
+  resources :password_resets, only: [ :new, :create, :edit, :update ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
