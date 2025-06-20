88bf8f540a6509ae0f0753e0e1acfb4873d3422a
dothihongkhanh <dothihongkhanh22@gmail.com>
2025-06-20 13:21:54 +0700

    chore: add gem letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   end
 
   resources :account_activations, only: [ :edit ]
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
